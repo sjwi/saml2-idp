27e61c46431b52cf7040c172fd8cde73562b75bf
Stephen Williams <stephenjw@fastmail.com>
2022-04-09 10:09:29 -0400

    Fix app constants

--- saml-idp/src/main/java/com/sjwi/saml/idp/config/ApplicationConstants.java
+++ saml-idp/src/main/java/com/sjwi/saml/idp/config/ApplicationConstants.java
@@ -9,16 +9,18 @@ import org.springframework.util.ResourceUtils;
 
 public class ApplicationConstants {
 
-	public static final String ISSUER_URI = "http://stephenky.com/saml-idp";
-	public static final String SLO_URL = "http://stephenky.com/saml-idp/slo/saml2";
-	public static final String SSO_URL = "http://stephenky.com/saml-idp/slo/saml2";
+	/*
+	 * The issuerUri can be set to whatever you want and will need to be passed to your service provider
+	 */
+	public static final String ISSUER_URI = "https://stephenky.com/saml-idp";
+	public static final String SLO_URL = "https://stephenky.com/saml-idp/slo/saml2";
+	public static final String SSO_URL = "https://stephenky.com/saml-idp/slo/saml2";
 	public static final String COMPANY = "StephenKY";
 	public static final String GIVEN_NAME = "Stephen";
 	public static final String SUR_NAME = "Williams";
 	public static final String OPERATIONS_EMAIL = "stephenjw@fastmail.com";
+	public static final String AUTHORIZATION_ENDPOINT = "https://stephenky.com/saml-idp/sso/saml2";
 	
-	public static final String AUTHORIZATION_ENDPOINT = "http://stephenky.com/saml-idp/sso/saml2";
-
 	public static final String PUBLIC_CERTIFICATE_FILE = "idp-public-cert.pem";
 
 	public static final String PRIVATE_KEY_FILE = "idp-private-key.der";
